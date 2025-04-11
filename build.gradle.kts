689b1ee83ea8684c20fd77877608d9ad15e78b36
Trương Minh Khôi <22520703@gm.uit.edu.vn>
2025-04-11 11:08:34 +0700

    Connect to DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,14 +19,19 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	// implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	// runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
--- src/main/java/com/example/pharmacywebsite/controller/HelloController.java
+++ src/main/java/com/example/pharmacywebsite/controller/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.pharmacywebsite.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pharmacywebsite.domain.Person;
+import com.example.pharmacywebsite.repository.PersonRepo;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
-    public String getHelloWorld() {
-        return "Hello Customer";
+
+    private final PersonRepo repo;
+
+    public HelloController(PersonRepo repo) {
+        this.repo = repo;
+    }
+
+    @PostMapping("/addPerson")
+    public Person addPerson(@RequestBody Person p) {
+        return repo.save(p);
     }
 }
