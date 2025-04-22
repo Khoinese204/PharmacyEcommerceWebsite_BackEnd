77785c05d21ca68aec25c2a5c52e59da22d30571
Trương Minh Khôi <22520703@gm.uit.edu.vn>
2025-04-23 01:33:18 +0700

    cors

--- src/main/java/com/example/pharmacywebsite/controller/HelloController.java
+++ src/main/java/com/example/pharmacywebsite/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.pharmacywebsite.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,4 +23,6 @@ public class HelloController {
     public Person addPerson(@RequestBody Person p) {
         return repo.save(p);
     }
+
+
 }
