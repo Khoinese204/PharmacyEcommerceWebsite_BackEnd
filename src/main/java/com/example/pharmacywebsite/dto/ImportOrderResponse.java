64adf791cb0938d40c5ccf930a50db3c392aa801
khoaphamlee <plkhoa0111@gmail.com>
2025-11-20 22:32:51 +0700

    fix: import many medicines

--- src/main/java/com/example/pharmacywebsite/dto/ImportOrderResponse.java
+++ src/main/java/com/example/pharmacywebsite/dto/ImportOrderResponse.java
@@ -2,6 +2,7 @@ package com.example.pharmacywebsite.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,7 @@ public class ImportOrderResponse {
     private Integer id;
     private String supplierName;
     private Double totalPrice;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt;
     private List<ImportOrderItemResponse> items;
 }
